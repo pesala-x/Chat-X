b4f30a051ded8b55e13b3268441969b73658ac05
pesala-x <gkpesalawinodith@gmail.com>
2024-01-20 19:07:36 +0530

    ClientHandler util package done...

--- src/main/java/lk/pesala_x/ClientUtil/ClientHandler.java
+++ src/main/java/lk/pesala_x/ClientUtil/ClientHandler.java
@@ -1,5 +1,51 @@
 package lk.pesala_x.ClientUtil;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.List;
+
 public class ClientHandler {
+
+    Socket socket;
+
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+    private List<ClientHandler> client;
+    String msg;
+
+
+    public ClientHandler(Socket socket, List<ClientHandler> clients){
+
+        try {
+            this.socket = socket;
+            this.client = clients;
+            this.dataInputStream = new DataInputStream(socket.getInputStream());
+            this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (socket.isConnected()) {
+                        msg = dataInputStream.readUTF();
+                        for (ClientHandler clientHandler : clients) {
+                            if (clientHandler.socket.getPort() != socket.getPort()) {
+                                clientHandler.dataOutputStream.writeUTF(msg);
+                                clientHandler.dataOutputStream.flush();
+                            }
+                        }
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
 
 }
