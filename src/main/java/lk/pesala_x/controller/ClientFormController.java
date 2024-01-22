48875a4481860c93391e726baff229ad4b090c85
pesala-x <gkpesalawinodith@gmail.com>
2024-01-22 23:00:07 +0530

    Chat application All features are Updated..

--- src/main/java/lk/pesala_x/controller/ClientFormController.java
+++ src/main/java/lk/pesala_x/controller/ClientFormController.java
@@ -360,5 +360,8 @@ public class ClientFormController implements Initializable {
         name = namtext.getText();
     }
 
+    private void printName(String name){
+        nam = name;
+    }
 
 }
